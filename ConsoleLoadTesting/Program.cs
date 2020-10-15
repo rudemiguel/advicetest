d5cce763b11039fd2d4c8bcc2be0ba9eb2d69ebf
Кирилл Пономарёв <PonomarevKY@it2g.ru>
2020-10-15 13:46:27 +0500

    NBomber

---
+++ ConsoleLoadTesting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+
+namespace ConsoleLoadTesting
+{
+    public class Program
+    {
+        private static string Host => "http://localhost:5000/";
+        private static int WarmUpDurationInSeconds => 1;
+        private static int DurationInSeconds => 20;
+        private static int RequestInOneSecond => 200;
+        
+        private static void Main(string[] args)
+        {
+            var simpleStep = HttpStep.Create("simple_step",
+                context => Http.CreateRequest("GET", $"{Host}WeatherForecast/Simple"));
+            
+            var aspectStep = HttpStep.Create("aspect_step",
+                context => Http.CreateRequest("GET", $"{Host}WeatherForecast"));
+            
+            var scenario = ScenarioBuilder.CreateScenario("scenario", simpleStep, aspectStep)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(WarmUpDurationInSeconds))
+                .WithLoadSimulations(Simulation.InjectPerSec(RequestInOneSecond, TimeSpan.FromSeconds(DurationInSeconds)));
+            
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .Run();
+        }
+    }
+}
--- advicetest/Advices/ApplicationServiceAdvice.cs
+++ advicetest/Advices/ApplicationServiceAdvice.cs
@@ -7,44 +7,44 @@ using advicetest.Infrastructure.Interceptors;
 
 namespace advicetest.Advices
 {
-	/// <summary>
-	/// Адвайз для сервисов приложения
-	/// </summary>
-	public class ApplicationServiceAdvice : IAdvice<IApplicationService>
-	{
-		private readonly ILogger<ApplicationServiceAdvice> _logger;
+    /// <summary>
+    /// Адвайз для сервисов приложения
+    /// </summary>
+    public class ApplicationServiceAdvice : IAdvice<IApplicationService>
+    {
+        private readonly ILogger<ApplicationServiceAdvice> _logger;
 
-		/// <summary>
-		/// Адвайз для сервисов приложения
-		/// </summary>
-		public ApplicationServiceAdvice(
-			ILogger<ApplicationServiceAdvice> logger
-		)
-		{
-			_logger = logger;
-		}
+        /// <summary>
+        /// Адвайз для сервисов приложения
+        /// </summary>
+        public ApplicationServiceAdvice(
+            ILogger<ApplicationServiceAdvice> logger
+        )
+        {
+            _logger = logger;
+        }
 
-		/// <summary>
-		/// Обработчик перед выполнением метода
-		/// </summary>
-		public async Task Before(MethodInfo methodInfo, object[] parameters)
-		{
-			_logger.LogInformation("Перед вызовом метода {@methodName}", methodInfo.Name);
-		}
+        /// <summary>
+        /// Обработчик перед выполнением метода
+        /// </summary>
+        public async Task Before(MethodInfo methodInfo, object[] parameters)
+        {
+            _logger.LogInformation("Перед вызовом метода {@methodName}", methodInfo.Name);
+        }
 
-		/// <summary>
-		/// Обработчик после выполнения метода
-		/// </summary>
-		public async Task After(MethodInfo methodInfo, object returnValue, object[] parameters)
-		{
-			_logger.LogInformation("После вызовом метода {@methodName}", methodInfo.Name);
-		}
+        /// <summary>
+        /// Обработчик после выполнения метода
+        /// </summary>
+        public async Task After(MethodInfo methodInfo, object returnValue, object[] parameters)
+        {
+            _logger.LogInformation("После вызовом метода {@methodName}", methodInfo.Name);
+        }
 
-		/// <summary>
-		/// Обработчик при исключении
-		/// </summary>
-		public async Task Exception(MethodInfo methodInfo, object[] parameters, Exception exception)
-		{
-		}
-	}
+        /// <summary>
+        /// Обработчик при исключении
+        /// </summary>
+        public async Task Exception(MethodInfo methodInfo, object[] parameters, Exception exception)
+        {
+        }
+    }
 }
