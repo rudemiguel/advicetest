c39322cb30b95ff66c06e093b0e7e6b68bea34cf
Кирилл Пономарёв <PonomarevKY@it2g.ru>
2020-10-15 14:36:46 +0500

    Nbomber refactor

--- ConsoleLoadTesting/Program.cs
+++ ConsoleLoadTesting/Program.cs
@@ -8,23 +8,27 @@ namespace ConsoleLoadTesting
     {
         private static string Host => "http://localhost:5000/WeatherForecast/";
         private static int WarmUpDurationInSeconds => 1;
-        private static int DurationInSeconds => 10;
-        private static int RequestInOneSecond => 200;
+        private static int DurationInSeconds => 30;
+        private static int RequestInOneSecond => 500;
         
         private static void Main(string[] args)
         {
             var simpleStep = HttpStep.Create("simple_step",
                 context => Http.CreateRequest("GET", $"{Host}Simple"));
             
+            var simpleScenario = ScenarioBuilder.CreateScenario("simple_scenario", simpleStep)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(WarmUpDurationInSeconds))
+                .WithLoadSimulations(Simulation.InjectPerSec(RequestInOneSecond, TimeSpan.FromSeconds(DurationInSeconds)));            
+            
             var aspectStep = HttpStep.Create("aspect_step",
                 context => Http.CreateRequest("GET", $"{Host}LoadTesting"));
             
-            var scenario = ScenarioBuilder.CreateScenario("scenario", simpleStep, aspectStep)
+            var aspectScenario = ScenarioBuilder.CreateScenario("aspect_scenario", aspectStep)
                 .WithWarmUpDuration(TimeSpan.FromSeconds(WarmUpDurationInSeconds))
                 .WithLoadSimulations(Simulation.InjectPerSec(RequestInOneSecond, TimeSpan.FromSeconds(DurationInSeconds)));
             
             NBomberRunner
-                .RegisterScenarios(scenario)
+                .RegisterScenarios(simpleScenario, aspectScenario)
                 .Run();
         }
     }
