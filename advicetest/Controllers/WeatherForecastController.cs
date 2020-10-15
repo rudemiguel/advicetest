adf6f8c5b8ab11db98ff89496def9dc94aeef8a4
Кирилл Пономарёв <PonomarevKY@it2g.ru>
2020-10-15 12:56:59 +0500

    Вызов внутри контроллера

--- advicetest/Controllers/WeatherForecastController.cs
+++ advicetest/Controllers/WeatherForecastController.cs
@@ -11,18 +11,25 @@ namespace advicetest.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly IWeatherForecastService _weatherForecastService;
+        private readonly ISimpleWeatherForecastService _simpleWeatherForecastService;
 
         public WeatherForecastController(
-            IWeatherForecastService weatherForecastService
-        )
+            IWeatherForecastService weatherForecastService, ISimpleWeatherForecastService simpleWeatherForecastService)
         {
             _weatherForecastService = weatherForecastService;
+            _simpleWeatherForecastService = simpleWeatherForecastService;
         }
 
         [HttpGet]
         public async Task<IEnumerable<WeatherForecastContract>> Get()
         {
             return await _weatherForecastService.GetForecast(10);
+        }
+        
+        [HttpGet("Simple")]
+        public async Task<IEnumerable<WeatherForecastContract>> GetSimple()
+        {
+            return await _simpleWeatherForecastService.GetForecast(10);
         }
     }
 }
